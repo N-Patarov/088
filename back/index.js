03abfc404fa437f9fdf7bcd72c884fcad1aa08bd
niki <nikolayp1707@gmail.com>
2022-06-11 19:39:41 +0300

    add api to url

--- back/index.js
+++ back/index.js
@@ -27,10 +27,6 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
     next();
   });
 
